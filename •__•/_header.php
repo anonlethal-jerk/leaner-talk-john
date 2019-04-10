ed6a7aac959e134d886da09187e0e5a01e2b6c93
a Nonlethal Jerk <jk@jk-keller.com>
2019-04-10 05:01:39 -0400

    rebuilding the jenga tower to knock it down again later

--- •__•/_header.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title><?= $page_title; ?></title>
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="/•__•/o__o/main.css" type="text/css">
-  </head>
-  <body class="<?= $page_slug; ?>">

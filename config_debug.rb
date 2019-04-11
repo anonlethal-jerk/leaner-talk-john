2af4065230b4830f9db2e83f2d02e5ad9ae6723c
a Nonlethal Jerk <jk@jk-keller.com>
2019-04-11 08:12:28 -0400

    configuring my style

---
+++ config_debug.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = ""
+css_dir = "o__o"
+sass_dir = "o__o"
+images_dir = "x__x"
+javascripts_dir = "*__*"
+
+output_style = :expanded
+line_comments = true

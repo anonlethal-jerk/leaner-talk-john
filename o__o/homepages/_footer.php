14fa5eef55de3f4b6a183a1526a27a9711d45c00
A Nonlethal Jerk <jk@jk-keller.com>
2019-10-14 15:57:20 -0400

    fix your absolute views

--- o__o/homepages/_footer.php
+++ o__o/homepages/_footer.php
@@ -1,4 +1,4 @@
-      <a href="/index-list.php" class="" id="groundhog" style="display:block;position: absolute;top: 16px;left: 16px;z-index: 9999;background-color: #000;color: #fff;padding: 1rem 1.25rem;box-shadow: 0 0 30px 5px #c71123;font-size: 20px;transition: top 10s, left 10s, box-shadow 10s;">TKJKTK</a>
+      <a href="/index-list.php" class="" id="groundhog" style="display:block;position: fixed;top: 16px;left: 16px;z-index: 9999;background-color: #000;color: #fff;padding: 1rem 1.25rem;box-shadow: 0 0 30px 5px #c71123;font-size: 20px;transition: top 10s, left 10s, box-shadow 10s;">TKJKTK</a>
 
       <script type="text/javascript">
         // No more clicks

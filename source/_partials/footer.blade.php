1c361e1dc0487b2c3990236f51413fc2412dfd43
a NonLethal Jerk <anonlethaljerk@gmail.com>
2020-03-01 17:58:24 -0500

    plain jane, vanilla gorilla

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -2,8 +2,6 @@
 	<!--   being decent   -->
 	<nav id="groundhog" class="hog"><a href="/">JK</a></nav>
 
-	<script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
-	<script>window.jQuery || document.write('<script src="{{ mix('*__*/jquery-3.4.0.min.js', '') }}"><\/script>')</script>
 	<script src="{{ mix('*__*/global.js', '') }}" async=""></script>
 	<script src="{{ mix('*__*/tables.js', '') }}" async=""></script>
 	<script src="{{ mix('*__*/image_viewer.js', '') }}" async=""></script>

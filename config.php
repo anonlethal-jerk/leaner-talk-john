de0e0fa5169071b3274cd1cda7f2b19782304077
a NonLethal Jerk <anonlethaljerk@gmail.com>
2020-03-15 11:21:48 -0400

    No, I want it to be my version!

--- config.php
+++ config.php
@@ -4,10 +4,27 @@ return [
 	'production' => false,
 	'baseUrl' => '',
 	'collections' => [
+		'posts' => [
+			'author' => 'JK',
+			'sort' => '-date',
+			'path' => 'o__-/{filename}',
+		],
 		'boxbots',
 		'submissions',
 		'daily_photo',
 		'dirty_mousepads',
-		'dots_dots_dots'
+		'dots_dots_dots',
+
+		'arrow_iterations',
+		'flat_mountains',
+		'fucking_windows',
+		'homepages',
+		'iphone_oil_paintings',
+		'layered_wax',
+		'profiles_exchanged',
+		'profiles_reversed',
+		'sd_to_hd',
+		'siri_sound_poems',
+		'touching_film',
 	],
 ];
